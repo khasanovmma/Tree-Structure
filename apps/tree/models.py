4d2c0aa0b5f18446d62b3cb56031ceca9af23a61
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2024-09-14 06:39:05 +0500

    feat: implement organization and employee models

---
+++ apps/tree/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+from apps.core.models.abstract import CreatedUpdatedAbstractModel
+
+
+class Organization(MPTTModel, CreatedUpdatedAbstractModel):
+    name = models.CharField(max_length=255)
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, related_name="children", null=True, blank=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Employee(CreatedUpdatedAbstractModel):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    hire_date = models.DateField()
+    salary = models.DecimalField(max_digits=12, decimal_places=2)
+    organization = models.ForeignKey(
+        "tree.Organization", on_delete=models.CASCADE, related_name="employees"
+    )
+
+    def __str__(self):
+        return self.full_name
